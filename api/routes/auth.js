901c4fd0e8f6db12112973553d52479cb4667448
unknown <Dunghuvy@gmail.com>
2024-05-24 23:28:18 +0700

    Create 1st api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.get("/", (req,res)=>{
     res.send("hello, this is auth endpoint")
 })
+router.get("/register", (req,res)=>{
+    res.send("hello, this is auth register endpoint")
+})
 
 export default router
